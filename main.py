ae8e293d918cee6ee977afb5e2a0454e756475c7
kimjunhhee <sn2025056@shs.hs.kr>
2025-06-10 14:49:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 st.title("나의 첫 app")
 st.write("hello")
+name=st.text_input("이름 입력")
+if name:
+  if name=="김준희":
+    st.succcess("반갑습니다. 김준희님!")
+  else:
+    st.warning("누구세요?)
